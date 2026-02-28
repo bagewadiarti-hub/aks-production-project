d887dc83e2e69701e1477cacae45cd9b04354b0f
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-02-28 12:39:16 +0530

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tf-rg"
-    storage_account_name = "tfstateprod123"
+    storage_account_name = "tfstorageprod177"
     container_name       = "tfstate"
     key                  = "aks-prod.tfstate"
   }
