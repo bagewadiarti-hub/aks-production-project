a94c152ee74784ed76e162543220620b4a970b8e
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-02-28 18:15:25 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
-  resource_group_name = var.rg_name
-  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+location            = azurerm_resource_group.rg.location
   sku                 = "Standard"
   admin_enabled       = true
 }
