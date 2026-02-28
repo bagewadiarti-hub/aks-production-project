974655d43b26f07143677ca7946f2dbea9b9e473
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-02-28 12:47:31 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -12,18 +12,18 @@ resource "azurerm_container_registry" "acr" {
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "aksdns"
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = var.rg_name
+  dns_prefix          = var.dns_prefix
 
   default_node_pool {
-    name                = "systempool"
-    vm_size             = var.vm_size
-    node_count          = var.node_count
+    name                = "system"
+    vm_size             = "Standard_DS2_v2"
+
     enable_auto_scaling = true
     min_count           = 1
-    max_count           = 5
+    max_count           = 3
   }
 
   identity {
